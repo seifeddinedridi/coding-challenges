4038835c785c7f34fca3403df96b66541ddda747
Seifeddine Dridi <seifeddine.dridi89@gmail.com>
2024-12-11 18:20:28 +0100

    Create Binary TreeLevelOrderTraversal.java

---
+++ src/main/java/dev/seifeddinedridi/codingchallenges/Binary TreeLevelOrderTraversal.java
@@ -0,0 +1,33 @@
+package dev.seifeddinedridi.codingchallenges;
+
+public class BinaryTreeLevelOrderTraversal {
+  public List<List<Integer>> levelOrder(TreeNode root) {
+      if (root == null) {
+          return List.of();
+      }
+      var ans = new ArrayList<List<Integer>>();
+      var queue = new LinkedList<TreeNode>();
+      queue.add(root);
+      ans.add(List.of(root.val));
+      while (!queue.isEmpty()) {
+          var secondaryQueue = new LinkedList<TreeNode>();
+          var values = new ArrayList<Integer>();
+          while (!queue.isEmpty()) {
+              var node = queue.poll();
+              if (node.left != null) {
+                  secondaryQueue.add(node.left);
+                  values.add(node.left.val);
+              }
+              if (node.right != null) {
+                  secondaryQueue.add(node.right);
+                  values.add(node.right.val);
+              }
+          }
+          queue.addAll(secondaryQueue);
+          if (!values.isEmpty()) {
+              ans.add(values);
+          }
+      }
+      return ans;
+  }
+}
