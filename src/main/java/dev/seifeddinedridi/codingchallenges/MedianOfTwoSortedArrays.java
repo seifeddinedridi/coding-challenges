a233915df2eeef5e4daa228d59849a385c8c0d7b
Seifeddine Dridi <seifeddine.dridi89@gmail.com>
2025-07-26 08:20:53 +0200

    Add files via upload

--- src/main/java/dev/seifeddinedridi/codingchallenges/MedianOfTwoSortedArrays.java
+++ src/main/java/dev/seifeddinedridi/codingchallenges/MedianOfTwoSortedArrays.java
@@ -5,6 +5,33 @@ import java.util.Arrays;
 public class MedianOfTwoSortedArrays {
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return (findMedianSortedArrays(nums1, 0, nums2, 0, (nums1.length + nums2.length + 1) / 2)
+                + findMedianSortedArrays(nums1, 0, nums2, 0, (nums1.length + nums2.length + 2) / 2)) / 2.0;
+    }
+
+    private double findMedianSortedArrays(int[] nums1, int start1, int[] nums2, int start2, int k) {
+        if (start1 >= nums1.length) {
+            return nums2[start2 + k - 1];
+        } else if (start2 >= nums2.length) {
+            return nums1[start1 + k - 1];
+        }
+        if (k == 1) {
+            return Math.min(nums1[start1], nums2[start2]);
+        }
+        var median1 = start1 + k / 2 - 1;
+        median1 = median1 >= nums1.length ? Integer.MAX_VALUE : nums1[median1];
+        var median2 = start2 + k / 2 - 1;
+        median2 = median2 >= nums2.length ? Integer.MAX_VALUE : nums2[median2];
+        if (median1 < median2) {
+            // Keep right half of A and left half of B
+            return findMedianSortedArrays(nums1, start1 + k / 2, nums2, start2,  k - k / 2);
+        } else {
+            // Keep left half of A and right half of B
+            return findMedianSortedArrays(nums1, start1, nums2, start2 + k / 2, k - k / 2);
+        }
+    }
+
+    public double findMedianSortedArraysSlow(int[] nums1, int[] nums2) {
         int index1 = 0, index2 = 0;
         int value1 = 0, value2 = 0;
         for (int i = 0; i <= (nums1.length + nums2.length) / 2; i++) {
