6abe79304f0e3efd034747428c7fe97d8756addb
Seifeddine Dridi <seifeddine.dridi89@gmail.com>
2025-10-04 16:18:59 +0200

    Update CloseStrings.java

--- src/main/java/dev/seifeddinedridi/codingchallenges/CloseStrings.java
+++ src/main/java/dev/seifeddinedridi/codingchallenges/CloseStrings.java
@@ -8,42 +8,41 @@ public class CloseStrings {
         if (word1.length() != word2.length()) {
             return false;
         }
-        var word1Map = new HashMap<Character, Integer>();
-        var word2Map = new HashMap<Character, Integer>();
+        var word1Map = new int[26];
+        var word2Map = new int[26];
         for (var i = 0; i < word1.length(); i++) {
             var c1 = Character.valueOf(word1.charAt(i));
-            word1Map.compute(c1, (k, v) -> {
-                if (v == null) {
-                    return 1;
-                }
-                return v + 1;
-            });
+            word1Map[c1 - 'a']++;
             var c2 = Character.valueOf(word2.charAt(i));
-            word2Map.compute(c2, (k, v) -> {
-                if (v == null) {
-                    return 1;
-                }
-                return v + 1;
-            });
+            word2Map[c2 - 'a']++;
         }
-        if (!word1Map.keySet().equals(word2Map.keySet())) {
-            return false;
-        }
-        var reverseWord1Map = new HashMap<Integer, Integer>();
-        for (var e : word1Map.entrySet()) {
-            reverseWord1Map.compute(e.getValue(), (k, v) -> {
-                if (v == null) {
-                    return 1;
-                }
-                return v + 1;
-            });
-        }
-        for (var e : word2Map.entrySet()) {
-            if (!reverseWord1Map.containsKey(e.getValue())
-                    || reverseWord1Map.get(e.getValue()) == 0) {
+        for (var i = 0; i < word1Map.length; i++) {
+            if ((word1Map[i] == 0 && word2Map[i] != 0)
+            || word1Map[i] != 0 && word2Map[i] == 0) {
                 return false;
             }
-            reverseWord1Map.compute(e.getValue(), (k, v) -> {
+        }
+        var reverseWord1Map = new HashMap<Integer, Integer>();
+        for (var i = 0; i < word1Map.length; i++) {
+            if (word1Map[i] == 0) {
+                continue;
+            }
+            reverseWord1Map.compute(word1Map[i], (k, v) -> {
+                if (v == null) {
+                    return 1;
+                }
+                return v + 1;
+            });
+        }
+        for (var i = 0; i < word2Map.length; i++) {
+            if (word2Map[i] == 0) {
+                continue;
+            }
+            if (!reverseWord1Map.containsKey(word2Map[i])
+                || reverseWord1Map.get(word2Map[i]) == 0) {
+                return false;
+            }
+            reverseWord1Map.compute(word2Map[i], (k, v) -> {
                 if (v == 1) {
                     return null;
                 }
