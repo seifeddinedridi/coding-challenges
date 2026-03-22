3ccb9a0b7e9c1f9add7e0eb9a873a073a0c063c5
seifeddine <seifeddine.dridi89@gmail.com>
2026-03-22 13:56:39 +0100

    Added new challenges

---
+++ src/main/java/dev/seifeddinedridi/codingchallenges/DetectCycleInLinkedList2.java
@@ -0,0 +1,27 @@
+package dev.seifeddinedridi.codingchallenges;
+
+public class DetectCycleInLinkedList2 {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        var slow = head;
+        var fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
